726e1b0aab4dfc63ffbb891a418eae4698eacd34
“Jayce” <15332019087@163.com>
2023-12-12 17:56:13 +0800

    可视化左侧完成

--- src/views/screen/components/age/index.vue
+++ src/views/screen/components/age/index.vue
@@ -1,9 +1,96 @@
 <template>
-  <div class="">年龄</div>
+  <div class="age">
+    <div class="title">
+      年龄比例
+      <p></p>
+    </div>
+    <div class="charts" ref="charts">
+
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref, onMounted } from "vue"
+import * as echarts from 'echarts';
+let charts = ref()
+onMounted(() => {
+  let myChart = echarts.init(charts.value)
+  let options = {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      top: '5%',
+      right: '5%',
+      bottom: '5%',
+      orient: 'vertical',
+      textStyle: {
+        color: "white"
+      }
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: '#fff',
+          borderWidth: 2
+        },
+        label: {
+          show: true,
+          position: 'inside'
+        },
+
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: 1048, name: '军事' },
+          { value: 735, name: '经济' },
+          { value: 580, name: '人口' },
+          { value: 484, name: '财经' },
+          { value: 300, name: '城市' }
+        ],
+        left: '-30%'
+      }
+    ],
+    grid: {
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0
+    }
+  };
+  myChart.setOption(options)
+})
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.age {
+  background: url(../../images/dataScreen-main-lb.png) no-repeat;
+  background-size: cover;
+
+  .title {
+    font-size: 24px;
+    color: white;
+    padding: 20px;
+
+    p {
+      margin-top: 5px;
+      background: url(../../images/dataScreen-title.png) no-repeat;
+      background-size: cover;
+      width: 68px;
+      height: 7px;
+    }
+  }
+
+  .charts {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
--- src/views/screen/components/sex/index.vue
+++ src/views/screen/components/sex/index.vue
@@ -1,9 +1,93 @@
 <template>
-  <div class="">性别</div>
+  <div class="sex">
+    <div class="title">
+      男女比例
+      <p></p>
+    </div>
+    <div class="rate">
+      <div class="man">
+        <img src="../../images/man.png" alt="">
+      </div>
+      <div class="woman">
+        <img src="../../images/woman.png" alt="">
+      </div>
+    </div>
+    <div class="line">
+      <div class="line_title">
+        <div>男士58%</div>
+        <div>女士42%</div>
+      </div>
+      <el-progress define-back-color="red" color="#138FE2" :show-text="false" :text-inside="true" :stroke-width="20"
+        :percentage="58" status="exception" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.sex {
+  padding: 20px;
+  background: url(../../images/dataScreen-main-lc.png) no-repeat;
+  background-size: 100% 100%;
+  margin: 15px 0;
+
+  .rate {
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: 25px;
+
+    .man {
+      width: 111px;
+      height: 116px;
+      background: Url(../../images/man-bg.png) no-repeat;
+      background-size: cover;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        width: 60px;
+        height: 64px;
+      }
+    }
+
+    .woman {
+      width: 111px;
+      height: 116px;
+      background: url(../../images/woman-bg.png) no-repeat;
+      background-size: cover;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        width: 60px;
+        height: 64px;
+      }
+
+    }
+  }
+
+  .line {
+
+    // display: flex;
+    // justify-content: space-evenly;
+    .line_title {
+      margin: 20px 0;
+      color: white;
+      display: flex;
+      justify-content: space-evenly;
+      font-size: 18px;
+    }
+
+
+  }
+
+  .el-progress-bar__outer {
+    height: 10px !important;
+    background-color: #f2994a !important;
+  }
+}
+</style>
